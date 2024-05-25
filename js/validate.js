b0d45036dd6559c5b33389ea3e31c31293a1b5ca
= <leoulirodriguez@gmail.com>
2024-05-25 18:43:43 -0300

    validate login simple

--- js/validate.js
+++ js/validate.js
@@ -2,11 +2,38 @@
 //     alert('Se hizo clic en la página');
 // });
 
+const email = "test@gmail.com"
+const password = "test123"
+
 document.addEventListener('DOMContentLoaded', ()=>{
 
     // Obtengo Form Register
-    const form = document.querySelector('.section__form')
+    const section = document.querySelector('.section__container');
+    const form = document.querySelector('.section__form');
+    const form_inputs = document.querySelectorAll('.section__input');
+    const error_message = document.querySelector('.section__error')
+    console.log(form_inputs);
 
-    console.log(form)
+
+    form.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        const email_form = form.email.value.trim();
+        const password_form = form.password.value.trim();
+
+        console.log(email_form);
+        console.log(password_form);
+
+        if(email_form == '' || password_form == ''){
+            error_message.textContent = 'No pueden haber campos vacíos';
+    
+        }else if (password_form.length < 5){
+            error_message.textContent = 'Contraseña no puede tener menos de 5 carácteres';
+        }
+
+        if(email_form === email && password_form === password){
+            alert("Sesion iniciada");
+        }
+
+    })
 
 })
