b3bbf9dd155fa8b7fb7918b1dd22b3157ff23946
= <leoulirodriguez@gmail.com>
2024-05-25 17:23:14 -0300

    start validate.js

---
+++ js/validate.js
@@ -0,0 +1,12 @@
+// document.addEventListener('click', () => {
+//     alert('Se hizo clic en la página');
+// });
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    // Obtengo Form Register
+    const form = document.querySelector('.section__form')
+
+    console.log(form)
+
+})
